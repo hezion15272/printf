a0845a5b684d9bf2c1ac76226e6cf137b370059c
Hezekiah Ibok <77856161+hezion15272@users.noreply.github.com>
2021-07-27 21:37:52 +0100

    Update format_string.c

--- format_string.c
+++ format_string.c
@@ -56,6 +56,30 @@ int rot13(va_list list)
 }
 
 /**
+ * hex_print - prints a char's ascii value in uppercase hex
+ * @c: char to print
+ *
+ * Return: number of chars printed (always 2)
+ */
+static int hex_print(char c)
+{
+	int count;
+	char diff = 'A' - ':';
+	char d[2];
+
+	d[0] = c / 16;
+	d[1] = c % 16;
+	for (count = 0; count < 2; count++)
+	{
+		if (d[count] >= 10)
+			_putchar('0' + diff + d[count]);
+		else
+			_putchar('0' + d[count]);
+	}
+	return (count);
+}
+
+/**
  * print_S - prints a string and nonprintable character ascii values
  * @S: string to print
  *
