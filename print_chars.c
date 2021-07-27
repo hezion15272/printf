90b03566b2c2522a13a96c3da81a3baaeb8e346f
Hezekiah Ibok <77856161+hezion15272@users.noreply.github.com>
2021-07-27 21:35:04 +0100

    Update print_chars.c

--- print_chars.c
+++ print_chars.c
@@ -95,35 +95,5 @@ static int hex_print(char c)
 	return (count);
 }
 
-/**
- * print_S - prints a string and nonprintable character ascii values
- * @S: string to print
- *
- * Return: number of chars printed
- */
-int print_S(va_list S)
-{
-	unsigned int i;
-	int count = 0;
-	char *str = va_arg(S, char *);
 
-	if (str == NULL)
-		str = "(null)";
-	for (i = 0; str[i]; i++)
-	{
-		if (str[i] < 32 || str[i] >= 127)
-		{
-			_putchar('\\');
-			_putchar('x');
-			count += 2;
-			count += hex_print(str[i]);
-		}
-		else
-		{
-			_putchar(str[i]);
-			count++;
-		}
-	}
-	return (count);
-}
 
