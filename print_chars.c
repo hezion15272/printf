9274ee416b1dea41ce141edd1002ae461acc4890
Hezekiah Ibok <77856161+hezion15272@users.noreply.github.com>
2021-07-27 21:36:47 +0100

    Update print_chars.c

--- print_chars.c
+++ print_chars.c
@@ -70,30 +70,3 @@ int unsigned_integer(va_list list)
 		return (-1);
 	return (print_unsigned_number(num));
 }
-
-/**
- * hex_print - prints a char's ascii value in uppercase hex
- * @c: char to print
- *
- * Return: number of chars printed (always 2)
- */
-static int hex_print(char c)
-{
-	int count;
-	char diff = 'A' - ':';
-	char d[2];
-
-	d[0] = c / 16;
-	d[1] = c % 16;
-	for (count = 0; count < 2; count++)
-	{
-		if (d[count] >= 10)
-			_putchar('0' + diff + d[count]);
-		else
-			_putchar('0' + d[count]);
-	}
-	return (count);
-}
-
-
-
